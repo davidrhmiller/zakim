a86494c28b936a8a4c1699021885ce11b4f1368b
davidrhmiller <davidrhmiller@users.noreply.github.com>
2021-01-11 20:27:52 -0500

    Adds missing import to reporting.py

--- learning/tf/reporting.py
+++ learning/tf/reporting.py
@@ -1,6 +1,8 @@
 # This is patterned after the [TensorFlow custom training tutorial]
 # (https://www.tensorflow.org/tutorials/customization/custom_training_walkthrough#define_the_loss_and_gradient_function).
 
+import matplotlib.pyplot as plt
+
 def plot_training_experiment(train_loss_results, train_accuracy_results):
   fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
   fig.suptitle('Training Metrics')
